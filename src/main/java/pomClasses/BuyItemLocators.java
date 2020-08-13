7deca4cd297bd9e8494f8dc6d97c9f4b1d0bb643
taydin-ra <ttorun16@gmail.com>
2020-08-13 17:04:10 -0400

    cucumber hw 2 solution

--- src/main/java/pomClasses/BuyItemLocators.java
+++ src/main/java/pomClasses/BuyItemLocators.java
@@ -4,21 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import utilities.BaseDriver;
 
 public class BuyItemLocators extends BasePom {
     private WebDriver driver;
 
-    public BuyItemLocators(){
-        driver= BaseDriver.getDriver();
-        PageFactory.initElements(driver,this);
+    public BuyItemLocators() {
+        driver = BaseDriver.getDriver();
+        PageFactory.initElements(driver, this);
 
     }
+
     @FindBy(xpath = "(//a[@title='Dresses'])[2]")
 
     private WebElement dressesTab;
 
-    public void ClickOnDresses(){
+    public void ClickOnDresses() {
         clickOnFunctionalities(dressesTab);
     }
 
@@ -26,10 +28,119 @@ public class BuyItemLocators extends BasePom {
 
     private WebElement printedDress;
 
-    public void ClickOnPrintedDresses(){
+    public void ClickOnPrintedDresses() {
         ScrollDown(printedDress);
         clickOnFunctionalities(printedDress);
     }
+
+
+    @FindBy(css = "button[name='Submit']>span")
+
+    private WebElement addToCart;
+
+    @FindBy(xpath="//iframe[@class='fancybox-iframe']")
+
+    public WebElement myFrame;
+
+    public void clickOnAddToCart() {
+        driver.switchTo().frame(myFrame);
+        waitMethod();
+        clickOnFunctionalities(addToCart);
+    }
+
+    @FindBy(xpath = "//div[@class='button-container']//a")
+
+    private WebElement proceedToCheckOutFirst;
+
+    public void clickOnProceedToCheckOut() {
+        clickOnFunctionalities(proceedToCheckOutFirst);
+    }
+
+    @FindBy(xpath = "//p[@class='cart_navigation clearfix']//span")
+    private  WebElement proceedToCheckOutSecond;
+
+    @FindBy(css = "td#total_product")
+
+    private WebElement totalProducts;
+
+    @FindBy(css = "td#total_shipping")
+
+    private WebElement totalShipping;
+
+    @FindBy(css = "td#total_price")
+
+    private WebElement totalPrice;
+
+    @FindBy(css = "button[name='processAddress']")
+
+    private WebElement proceedToCheckOutLast;
+
+    @FindBy(css = "input#cgv")
+
+    private WebElement agreementCheckBox;
+
+    @FindBy(css = "a[title='Pay by bank wire']")
+
+    private WebElement paymentType;
+
+
+
+    @FindBy(css = "button[name='processCarrier']")
+
+    private WebElement shippingProceed;
+
+
+    @FindBy(xpath = "//span[text()='I confirm my order']")
+
+    private WebElement confirmation;
+
+    public void clickSecondProceed(){
+        clickOnFunctionalities(proceedToCheckOutSecond);
+    }
+
+    public void clickShippingProceed(){
+        clickOnFunctionalities(shippingProceed);
+    }
+
+    public void ClickOnPaymentType(){
+        clickOnFunctionalities(paymentType);
+    }
+
+    public void ClickOnConfirmation(){
+        clickOnFunctionalities(confirmation);
+    }
+    public void clickAgreementCheck(){
+        clickOnFunctionalities(agreementCheckBox);
+    }
+
+    public void clickOnLastProceed(){
+        clickOnFunctionalities(proceedToCheckOutLast);
+    }
+
+    public void verifyTotal() {
+        String  totalPr=totalProducts.getText();
+        String totalShip=totalShipping.getText();
+        String total=totalPrice.getText();
+
+        totalPr=totalPr.replace("$","");
+        totalShip=totalShip.replace("$","");
+        total=total.replace("$","");
+
+        double totalprice=Double.parseDouble(totalPr);
+        double totalshipping=Double.parseDouble(totalShip);
+
+        double Total=Double.parseDouble(total);
+
+        double allTotal=totalprice+totalshipping;
+
+        Assert.assertEquals(Total,allTotal);
+
+
+
+
+    }
+
+
 
 
 }
--- src/main/java/stepDefinition/BuyItemSteps.java
+++ src/main/java/stepDefinition/BuyItemSteps.java
@@ -24,52 +24,64 @@ public class BuyItemSteps extends BasePom {
     }
 
     @When("^Click on add to cart button$")
-    public void click_on_add_to_cart_button() throws Throwable {
+    public void click_on_add_to_cart_button()  {
+        buyItemLocator.clickOnAddToCart();
 
     }
 
     @When("^Click on proceed to checkout$")
-    public void click_on_proceed_to_checkout() throws Throwable {
+    public void click_on_proceed_to_checkout() throws InterruptedException {
+        Thread.sleep(3000);
+
+        buyItemLocator.clickOnProceedToCheckOut();
+        buyItemLocator.clickSecondProceed();
 
     }
 
     @Then("^Verify the total price equals total products and shipping$")
-    public void verify_the_total_price_equals_total_products_and_shipping() throws Throwable {
+    public void verify_the_total_price_equals_total_products_and_shipping() throws InterruptedException {
+        Thread.sleep(3000);
+        buyItemLocator.verifyTotal();
 
     }
 
     @Then("^Click on proceed to checkout in the order page$")
     public void click_on_proceed_to_checkout_in_the_order_page() throws Throwable {
-
+        buyItemLocator.clickOnProceedToCheckOut();
     }
 
     @Then("^Click on proceed to checkout in the address page$")
-    public void click_on_proceed_to_checkout_in_the_address_page() throws Throwable {
+    public void click_on_proceed_to_checkout_in_the_address_page()  {
+        buyItemLocator.clickOnLastProceed();
 
     }
 
     @Then("^Click on the agreement checkbox$")
-    public void click_on_the_agreement_checkbox() throws Throwable {
+    public void click_on_the_agreement_checkbox()  {
+        buyItemLocator.clickAgreementCheck();
 
     }
 
     @Then("^Click on the proceed to checkout button$")
-    public void click_on_the_proceed_to_checkout_button() throws Throwable {
+    public void click_on_the_proceed_to_checkout_button()  {
+        buyItemLocator.clickShippingProceed();
 
     }
 
     @Then("^Click on pay by bank wire$")
-    public void click_on_pay_by_bank_wire() throws Throwable {
+    public void click_on_pay_by_bank_wire(){
+        buyItemLocator.ClickOnPaymentType();
 
     }
 
     @Then("^Click on I confirm order$")
-    public void click_on_I_confirm_order() throws Throwable {
+    public void click_on_I_confirm_order()  {
+        buyItemLocator.ClickOnConfirmation();
 
     }
 
     @Then("^Verify Order confirmation text  is displayed$")
-    public void verify_Order_confirmation_text_is_displayed() throws Throwable {
+    public void verify_Order_confirmation_text_is_displayed()  {
 
     }
 }
